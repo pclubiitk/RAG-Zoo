5eb341381dc7d7542616727e1af8fa5084f9d7bf
Shubham Kumar Patel <shubhkrpatel@gmail.com>
2025-06-29 22:06:41 +0530

    Added gemini and open ai embedders

--- rag_src/embedder/__init__.py
+++ rag_src/embedder/__init__.py
@@ -1,7 +1,11 @@
 from .base import BaseEmbedder
 from .default import DefaultEmbedder
+from .gemini_embedder import GeminiEmbedder
+from .openai_embedder import OpenAIEmbedder
 
 __all__ = [
     "BaseEmbedder",
-    "DefaultEmbedder"
+    "DefaultEmbedder",
+    "GeminiEmbedder",
+    "OpenAIEmbedder"
 ]
