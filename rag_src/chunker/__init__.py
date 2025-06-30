4c1687eeaa8b0e2b38fc1459594571f29306208b
Priyanshu Ranjan <priyanshur24@iitk.ac.in>
2025-07-01 00:38:22 +0530

    Updated chunker init, resolver errors in recursive_splitter

--- rag_src/chunker/__init__.py
+++ rag_src/chunker/__init__.py
@@ -1,7 +1,13 @@
 from .base import BaseChunker
 from .default import DefaultChunker
+from .recursive_splitter import RecursiveChunker
+from .semantic_splitter import SemanticChunker
+from .text_splitter import TokenChunker
 
 __all__ = [
     "BaseChunker",
-    "DefaultChunker"
+    "DefaultChunker",
+    "RecursiveChunker",
+    "SemanticChunker",
+    "TokenChunker"
 ]
