9c38efb2f91c4510bf73b1d2869e8b3854abf64c
Shubham Kumar Patel <shubhkrpatel@gmail.com>
2025-07-02 18:35:46 +0530

    Added some unadded components to init

--- rag_src/chunker/__init__.py
+++ rag_src/chunker/__init__.py
@@ -1,7 +1,13 @@
 from .base import BaseChunker
 from .default import DefaultChunker
+from .recursive_splitter import RecursiveCharacterTextSplitter
+from .semantic_splitter import SemanticChunker
+from .text_splitter import TokenChunker
 
 __all__ = [
     "BaseChunker",
-    "DefaultChunker"
+    "DefaultChunker",
+    "RecursiveCharacterTextSplitter",
+    "SemanticChunker",
+    "TokenChunker"
 ]
