c22cd829419400abae50757a25b69cb7904360e9
Priyanshu Ranjan <priyanshur24@iitk.ac.in>
2025-07-02 18:02:55 +0530

    Resolved api issues

--- tests/geminitest.py
+++
@@ -1,5 +0,0 @@
-from llama_index.llms.google_genai import GoogleGenAI
-
-llm = GoogleGenAI(model="gemini-1.5-flash", api_key="AIzaSyDcWZvNjpQdzpv-XQ7Nz2ZzJKN4aVSEuug")
-resp = llm.complete("HI")
-print(str(resp))
