6086740db061e1be9aa1f595ff9c53fe14b7bac3
nideesh-iitk <nideesh.iitk@gmail.com>
2025-07-06 19:04:22 +0530

    Update contextual_compression.py

--- rag_src/postretrievaldoc_context_enricher/contextual_compression.py
+++ rag_src/postretrievaldoc_context_enricher/contextual_compression.py
@@ -1,62 +1,79 @@
-def replacetabwithspace(documentslist):
-  for doc in documentslist:
-    doc.page_content=doc.page_content.replace('/t', ' ')
-  return documentslist
+from rag_src.postretrievaldoc_context_enricher.base import BasePostRetrievalContextEnricher
+from typing import List
 
-import os
-os.makedirs('data', exist_ok=True)
+class ContextualCompression(BasePostRetrievalContextEnricher):
+    """
+    This module summarizes each document using the LLM
+    It helps reduce the size and the noise in the retrieved context.
+    """
 
-# Download the PDF document used in this notebook
-!wget -O data/Understanding_Climate_Change.pdf https://raw.githubusercontent.com/NirDiamant/RAG_TECHNIQUES/main/data/Understanding_Climate_Change.pdf
-!wget -O data/Understanding_Climate_Change.pdf https://raw.githubusercontent.com/NirDiamant/RAG_TECHNIQUES/main/data/Understanding_Climate_Change.pdf
-path = "data/Understanding_Climate_Change.pdf"
+    def __init__(self, llm):
+        self.llm = llm
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-def encode_pdf(path, chunk_size=1000, chunk_overlap=200):
+    def enrich(self, docs: List[str]) -> List[str]:
+      import os
+      import sys
+      from dotenv import load_dotenv
+      from langchain.retrievers.document_compressors import LLMChainExtractor
+      from langchain.retrievers import ContextualCompressionRetriever
+      from langchain.chains import RetrievalQA
 
-    loader = PyPDFLoader(path)
-    documents = loader.load()
+      from google.colab import userdata
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len
-    )
-    texts = text_splitter.split_documents(documents)
-    cleaned_texts = replacetabwithspace(texts)
+      GOOGLE_API_KEY=userdata.get("GOOGLE_API_KEY")
 
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY) # Specify the Gemini embedding model
-    vectorstore = FAISS.from_documents(cleaned_texts, embeddings)
+      def replacetabwithspace(documentslist):
+        for doc in documentslist:
+          doc.page_content=doc.page_content.replace('/t', ' ')
+        return documentslist
 
-    return vectorstore
+      import os
+      os.makedirs('data', exist_ok=True)
 
-vector_store = encode_pdf(path)
+      from langchain_google_genai import GoogleGenerativeAIEmbeddings
+      from langchain.document_loaders import PyPDFLoader
+      from langchain.text_splitter import RecursiveCharacterTextSplitter
+      from langchain_community.vectorstores import FAISS
+      def encode_pdf(path, chunk_size=1000, chunk_overlap=200):
 
-from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import LLMChainExtractor
-from langchain.chains import RetrievalQA
+        loader = PyPDFLoader(path)
+        documents = loader.load()
 
-retriever = vector_store.as_retriever()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len
+        )
+        texts = text_splitter.split_documents(documents)
+        cleaned_texts = replacetabwithspace(texts)
 
-llm_compressor = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0, google_api_key=GOOGLE_API_KEY)
-compressor = LLMChainExtractor.from_llm(llm_compressor)
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY) # Specify the Gemini embedding model
+        vectorstore = FAISS.from_documents(cleaned_texts, embeddings)
 
-compression_retriever = ContextualCompressionRetriever(
-    base_compressor=compressor,
-    base_retriever=retriever
-)
+        return vectorstore
 
-llm_qa_chain = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0, google_api_key=GOOGLE_API_KEY)
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm_qa_chain,
-    retriever=compression_retriever,
-    return_source_documents=True
-)
+      vector_store = encode_pdf(path)
 
-query = "What is the main topic of the document?"
-result = qa_chain.invoke({"query": query})
-print(result["result"])
-print(type(result["source_documents"]))
-print("Source documents:", result["source_documents"])
+      from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+      from langchain.retrievers import ContextualCompressionRetriever
+      from langchain.retrievers.document_compressors import LLMChainExtractor
+      from langchain.chains import RetrievalQA
+
+      retriever = vector_store.as_retriever()
+
+      llm_compressor = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0, google_api_key=GOOGLE_API_KEY)
+      compressor = LLMChainExtractor.from_llm(llm_compressor)
+
+      compression_retriever = ContextualCompressionRetriever(
+          base_compressor=compressor,
+          base_retriever=retriever
+      )
+
+      llm_qa_chain = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0, google_api_key=GOOGLE_API_KEY)
+      qa_chain = RetrievalQA.from_chain_type(
+        llm=llm_qa_chain,
+        retriever=compression_retriever,
+        return_source_documents=True
+      )
+
+      query = "What is the main topic of the document?"
+      result = qa_chain.invoke({"query": query})
+      return(result["source_documents"]))
